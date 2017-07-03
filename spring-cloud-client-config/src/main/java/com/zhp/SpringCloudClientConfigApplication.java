f7147344b70acb34e35d46bbce2dbad0f4e37b39
peng.zhou <1009042136@qq.com>
2017-07-03 17:27:59 +0800

    消息总线刷新配置

--- spring-cloud-client-config/src/main/java/com/zhp/SpringCloudClientConfigApplication.java
+++ spring-cloud-client-config/src/main/java/com/zhp/SpringCloudClientConfigApplication.java
@@ -2,8 +2,10 @@ package com.zhp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudClientConfigApplication {
 
 	public static void main(String[] args) {
